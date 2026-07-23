2a6cf3cbae75a6d33fc1c94e3ec969b288965c94
mintkim78 <mintkim78@gmail.com>
2026-07-23 17:19:20 -0400

    Fix boundary conditions for Y variable in mytest.py

--- mytest.py
+++ mytest.py
@@ -31,12 +31,12 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_w] and Y < 2:
+    if keys[pygame.K_w] and Y > -2:
         y -= 1
         Y -= 0.01
         dev.write(f":SOURce2:VOLTage:OFFSet {Y:.3f}")
         time.sleep(1/60)
-    if keys[pygame.K_s] and Y > -2:
+    if keys[pygame.K_s] and Y < 2:
         y += 1
         Y += 0.01
         dev.write(f":SOURce2:VOLTage:OFFSet {Y:.3f}")
