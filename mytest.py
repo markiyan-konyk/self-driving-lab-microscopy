a63105dd418726a84d9c30ffbbf3452e3ef39820
Marki-K <mark.konyk@gmail.com>
2026-07-23 18:30:36 -0400

    ahh

--- mytest.py
+++ mytest.py
@@ -19,7 +19,9 @@ dev = rm.open_resource('USB0::6833::1602::DG1ZA278M01038::0::INSTR')
 dev.timeout = 20000
 dev.query("*IDN?")
 dev.write(f"SOURce1:APPLy:DC 1,1,0")
+dev.write(f"SOURce2:APPLy:DC 1,1,0")
 dev.write(f"OUTP1 ON")
+dev.write(f"OUTP2 ON")
 X, Y = 0,0
 
 while running:
