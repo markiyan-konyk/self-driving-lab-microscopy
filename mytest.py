4b2d83b00e5c0684868a49f6e22af846120e76d1
Marki-K <mark.konyk@gmail.com>
2026-07-23 17:36:02 -0400

    ahh

--- mytest.py
+++ mytest.py
@@ -9,7 +9,7 @@ dev.timeout = 20000
 dev.query("*IDN?")
 dev.write(f"SOURce1:APPLy:DC 1,1,0")
 dev.write(f"OUTP1 ON")
-for x in range(1,10001,5):
+for x in range(1,10001,1000):
     print(f"{x} COMMANDS PER SECOND SENT")
     for y in range(x):
         i = y * (3/x)
