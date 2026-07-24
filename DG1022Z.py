95bd36506689277f7e7c8e2c78675e8bb53b473a
Marki-K <mark.konyk@gmail.com>
2026-07-24 11:44:44 -0400

    ahh

--- DG1022Z.py
+++ DG1022Z.py
@@ -52,7 +52,7 @@ class DG1022Z:
         
         self.rm = None
         self.device = None
-        self._open_debug
+        self._open_debug()
 
     def _open(self):
         self.rm = pyvisa.ResourceManager("@py")
